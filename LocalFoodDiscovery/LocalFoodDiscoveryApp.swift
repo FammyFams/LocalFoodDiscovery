75e5a3722435aba5dff7195eb671d42e2b732453
Matthew Zheng <matthewzhenghi@gmail.com>
2024-12-18 23:42:21 -0800

    Initial Commit

---
+++ LocalFoodDiscovery/LocalFoodDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalFoodDiscoveryApp.swift
+//  LocalFoodDiscovery
+//
+//  Created by Matthew Zheng on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalFoodDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalFoodDiscoveryTests/LocalFoodDiscoveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalFoodDiscoveryTests.swift
+//  LocalFoodDiscoveryTests
+//
+//  Created by Matthew Zheng on 12/18/24.
+//
+
+import Testing
+@testable import LocalFoodDiscovery
+
+struct LocalFoodDiscoveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
