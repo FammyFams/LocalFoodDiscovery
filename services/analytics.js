fad2aa830e945a518779867ff02ba09b3c959ac3
matthewzhenghi <matthewzhenghi@gmail.com>
2026-04-11 00:10:28 -0700

    Add device info super properties to PostHog analytics

--- services/analytics.js
+++ services/analytics.js
@@ -1,4 +1,7 @@
 import PostHog from 'posthog-react-native';
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
+import { Dimensions, Platform } from 'react-native';
 import { POSTHOG_API_KEY } from '../env';
 
 let posthog = null;
@@ -12,7 +15,24 @@ export function initAnalytics() {
     posthog = new PostHog(POSTHOG_API_KEY, {
       host: 'https://us.i.posthog.com',
     });
-    console.log('[Analytics] PostHog initialized');
+
+    const { width, height } = Dimensions.get('screen');
+    posthog.register({
+      device_brand: Device.brand,
+      device_model: Device.modelName,
+      os_name: Device.osName,
+      os_version: Device.osVersion,
+      app_version: Constants.expoConfig?.version ?? null,
+      app_build: Platform.OS === 'ios'
+        ? Constants.expoConfig?.ios?.buildNumber ?? null
+        : Constants.expoConfig?.android?.versionCode ?? null,
+      screen_width: width,
+      screen_height: height,
+      device_type: Device.deviceType,
+      is_physical_device: Device.isDevice,
+    });
+
+    console.log('[Analytics] PostHog initialized with device properties');
   } catch (e) {
     console.warn('[Analytics] PostHog init failed:', e);
   }
